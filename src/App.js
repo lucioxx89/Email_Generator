0db534a7d76a1d9cc3e02b5281afc234a937db7c
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-08 19:40:53 +0100

    Change title with h1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Email Generator</div>;
+  return (
+    <div className="App">
+      <h1>Email Generator</h1>
+    </div>
+  );
 }
 
 export default App;
