63a68bd68bf261af0fcfcbfed37bce3da3b4eac0
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-09 17:09:49 +0100

    length of string/email generated fixed but can be improved

--- src/components/PersonalEmail.js
+++ src/components/PersonalEmail.js
@@ -1,13 +1,19 @@
 function PersonalEmail() {
+  // OPTION 1
   let RandomEmail = " ";
+
+  const emailLength = 5 && 10;
 
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_+-;:,<>~!?@#$%^&*()";
-  for (var i = 0; i < characters.length; i++) {
+  for (var i = 0; i < emailLength; i++) {
     RandomEmail += characters.charAt(
       Math.floor(Math.random() * characters.length)
     );
   }
+
+  //     OPTION 2
+  //   const RandomEmail = Math.random().toString(36).substring(2, 10);
 
   return (
     <div>
