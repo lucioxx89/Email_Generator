2b009b3d82567aacefab7ec5784e9f9723f4a8bc
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-09 16:36:08 +0100

    generate a random string  but length of the string should be fixed

--- src/components/PersonalEmail.js
+++ src/components/PersonalEmail.js
@@ -1,7 +1,17 @@
 function PersonalEmail() {
+  let RandomEmail = " ";
+
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_+-;:,<>~!?@#$%^&*()";
+  for (var i = 0; i < characters.length; i++) {
+    RandomEmail += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
   return (
     <div>
-      <h2>Email</h2>
+      <h2> {RandomEmail} @facebook</h2>
     </div>
   );
 }
