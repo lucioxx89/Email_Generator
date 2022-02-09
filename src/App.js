b70fa2124cdd81f316f7e117e8dd7796d15aa0ef
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-09 15:58:58 +0100

    add Personal email component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import PersonalEmail from "./components/PersonalEmail";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Email Generator</h1>
+      <PersonalEmail></PersonalEmail>
     </div>
   );
 }
---
+++ src/components/PersonalEmail.js
@@ -0,0 +1,9 @@
+function PersonalEmail() {
+  return (
+    <div>
+      <h2>Email</h2>
+    </div>
+  );
+}
+
+export default PersonalEmail;
