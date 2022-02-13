682f37375f815ed42287d8d7a619e512546047f5
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-13 17:58:13 +0100

    add onClick button to icon, so far prints clicked when clicked

--- src/components/SuggestedEmail.js
+++ src/components/SuggestedEmail.js
@@ -2,10 +2,16 @@ function SuggestedEmail(props) {
   const alias = props.alias;
   const domain = props.domain;
 
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
+
   return (
     <>
       <div class="list-group-item w-10 " />
-      <i class="bi bi-clipboard"></i>
+      <button onClick={clickHandler}>
+        <i class="bi bi-clipboard"></i>
+      </button>
       {alias}@{domain}
     </>
   );
