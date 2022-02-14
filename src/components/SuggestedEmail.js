74ad72a18ec28d1b26aa161ee1c18a21bb3586bd
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-14 22:03:06 +0100

    styling

--- src/components/SuggestedEmail.js
+++ src/components/SuggestedEmail.js
@@ -13,7 +13,10 @@ function SuggestedEmail(props) {
   };
 
   return (
-    <div onClick={clickHandler} className="p-4 border-1  cursor-pointer">
+    <div
+      onClick={clickHandler}
+      className="p-4 border-1 border-radius cursor-pointer"
+    >
       <span className="font-bold">{alias}</span>@{domain}
       <i className={`bi bi-${copyIcon} pl-10`}></i>
     </div>
