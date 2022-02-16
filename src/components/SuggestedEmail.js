b13193ca6cdc3224beae637a323486e35c6c0db0
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-02-16 11:04:32 +0100

    add state for color, get green if clicked/copied

--- src/components/SuggestedEmail.js
+++ src/components/SuggestedEmail.js
@@ -5,17 +5,22 @@ function SuggestedEmail(props) {
   const domain = props.domain;
 
   const [copyIcon, setCopyIcon] = useState("clipboard");
+  const [colorBackground, setColorBackground] = useState(
+    "p-3 mb-2 bg-light text-dark cursor-pointer"
+  );
 
   const clickHandler = () => {
     navigator.clipboard.writeText(alias + "@" + domain);
-    setCopyIcon("check");
+    setCopyIcon("check-lg");
+    setColorBackground("p-3 mb-2 bg-green-500 text-white");
     console.log("Clicked");
   };
 
   return (
     <div
       onClick={clickHandler}
-      className="p-4 border-1 border-radius cursor-pointer"
+      className={colorBackground}
+      // className="p-4 border-1 border-radius cursor-pointer"
     >
       <span className="font-bold">{alias}</span>@{domain}
       <i className={`bi bi-${copyIcon} pl-10`}></i>
